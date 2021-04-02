b012294b3dd2a4602400f9b6b8f01456a6293335
section0000 <php2280@gmail.com>
2021-04-02 13:37:21 +0700

    abcxyz

---
+++ src/quanlycafemangdi/model/ThongTinDangNhap.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlycafemangdi.model;
+
+public class ThongTinDangNhap 
+{
+    private static String tenDangNhap;
+    private static String chucVu;
+    private static String matKhau;
+
+    public static String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public static void setTenDangNhap(String tenDangNhap) {
+        ThongTinDangNhap.tenDangNhap = tenDangNhap;
+    }
+
+    public static String getChucVu() {
+        return chucVu;
+    }
+
+    public static void setChucVu(String chucVu) {
+        ThongTinDangNhap.chucVu = chucVu;
+    }
+
+    public static String getMatKhau() {
+        return matKhau;
+    }
+
+    public static void setMatKhau(String matKhau) {
+        ThongTinDangNhap.matKhau = matKhau;
+    }
+
+    
+    
+}

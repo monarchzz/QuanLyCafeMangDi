766fb08dd1f6f34067e951220c29aa5a17ba9584
monarchzz <trunghieunguyendn@gmail.com>
2021-08-04 07:35:08 +0700

    change sender ID

--- src/quanlycafemangdi/speedsms/SendMessage.java
+++ src/quanlycafemangdi/speedsms/SendMessage.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class SendMessage {
     public static String defaultToken = "bpiwi7L6j6_43KgGUh8cKrxjtMseJPxy";
-    public static String defaultSenderID = "91985efdca75eaeb";
+    public static String defaultSenderID = "1d446d3d1b2e614d";
 
     public static void sendMessage(String token,String phoneNumber, String content, String senderID){
         SpeedSMSAPI api  = new SpeedSMSAPI(token);
